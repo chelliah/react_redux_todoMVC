c7d6949bcfcd46e1845e3a921f5845e2808e6e1b
Aron <athomas5@macalester.edu>
2016-02-05 17:01:56 -0600

    still debug

--- server/deploy.js
+++ server/deploy.js
@@ -1,15 +1,15 @@
-//since postinstall will also run when you npm install
-//locally we only make sure it runs in production
-if(process.env.NODE_ENV === 'production'){
+// Since postinstall will also run when you run npm install
+// locally we make sure it only runs in production
+if (process.env.NODE_ENV === 'production') {
 
-  //we create a child process that will run the production bundle command
+  // We basically just create a child process that will run
+  // the production bundle command
   var child_process = require('child_process');
-  child_process.exec('webpack -p --config ../webpack.production.config.js',
-    function(error, stdout, stderr){
-      console.log('stdout: ', stdout);
-      console.log('stderr: ', stderr);
-      if (error !== null) {
+  child_process.exec("webpack -p --config ../webpack.production.config.js", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
       console.log('exec error: ' + error);
-      }
-    })
+    }
+  });
 }
